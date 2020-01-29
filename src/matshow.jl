ab2dc46578b29b59441642bd73f1501fc2641da6
Lirimy <31124605+Lirimy@users.noreply.github.com>
2020-01-29 10:34:44 +0900

    fix storeimage

--- src/matshow.jl
+++ src/matshow.jl
@@ -15,7 +15,8 @@ end
 
 
 function matshow(A::AbstractMatrix; f::Function=x->clamp(1+floor(Int, 256*x), 1, 256))
-    ImageContainer{:jlc}(IndirectArray(mappedarray(f, A), CURRENT_COLORMAP))
+    #ImageContainer{:jlc}(IndirectArray(mappedarray(f, A), CURRENT_COLORMAP))
+    storeimage(:jlc, IndirectArray(mappedarray(f, A), CURRENT_COLORMAP))
 end
 
 
@@ -27,44 +28,43 @@ end
 function openanim(func::Function, fmt::Symbol=:gif)
     fin = Pipe()
     fout = IOBuffer()
-    
+
     if fmt == :mp4
         fproc = run(
             pipeline(`ffmpeg -v 0 -i - -pix_fmt yuv420p -f matroska -`,
                 stdin=fin, stdout=fout),
             wait=false)
-        
+
         try
             func(fin)
         finally
             close(fin)
         end
-        
+
         wait(fproc)
         close(fproc)
         return ImageContainer{:mp4}(take!(fout))
-    
+
     elseif fmt == :gif
         palette = tempname() * ".bmp"
         save(palette, IndirectArray(reshape(1:256, 16, 16)', SimpleHeatmaps.CURRENT_COLORMAP))
-    
+
         fproc = run(
             pipeline(`ffmpeg -v 0 -i - -i $palette -lavfi paletteuse=dither=sierra2_4a -f gif -`,
                 stdin=fin, stdout=fout),
             wait=false)
-        
+
         try
             func(fin)
         finally
             close(fin)
         end
-        
+
         wait(fproc)
         close(fproc)
         return ImageContainer{:gif}(take!(fout))
-    
+
     else
         error("Only support mp4 / gif: $fmt")
     end
 end
-
